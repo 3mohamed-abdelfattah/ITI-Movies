b926e6bfe8a0bfde6f76f846cce14b0d3d51de7a
Mohamed Mohamed <142848460+3mohamed-abdelfattah@users.noreply.github.com>
2024-09-14 21:58:24 +0300

    Update dataActions.jsx

--- src/middleware/dataActions.jsx
+++ src/middleware/dataActions.jsx
@@ -4,7 +4,7 @@ export const fetchMovies = () => async (dispatch) => {
   dispatch({ type: 'FETCH_MOVIES_REQUEST' });
 
   try {
-    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=ec9e0f68699c576a9904e295326db236&page=5`);
+    const { data } = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=ec9e0f68699c576a9904e295326db236&page=1`);
     dispatch({ type: 'FETCH_MOVIES_SUCCESS', payload: data.results });
   } catch (error) {
     dispatch({ type: 'FETCH_MOVIES_FAILURE', payload: error.message });
